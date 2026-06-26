14584ae9e36ed2f600510c64ebb8ec13dd776367
Michael Bright <1880109+mjbright@users.noreply.github.com>
2026-06-26 17:37:47 +0200

    Added some CMD & files checks in build-scripts/build-docker-image.sh

--- build-scripts/build-docker-image.sh
+++ build-scripts/build-docker-image.sh
@@ -3,12 +3,12 @@ TAG="debian-trixie-installer"
 
 BUILD_ON_AMD64() {
     echo; echo "BUILD_ON_AMD64: building Docker image $TAG ..."
-    docker build -t $TAG .
+    set -x; docker build -t $TAG .; set +x
 }
 
 BUILD_ON_ARM64() {
     echo; echo "BUILD_ON_ARM64: building Docker image $TAG ..."
-    docker buildx create --name mybuilder --use
+    set -x; docker buildx create --name mybuilder --use; set +x
 
     # UNNEEDED NOW: Make "buildx" the default
     #docker buildx install
@@ -16,7 +16,14 @@ BUILD_ON_ARM64() {
     # Build for multiple platforms
     #docker build --platform linux/amd64,linux/arm64 -t $TAG .
     # Build for AMD64 platform:
-    docker build --load --platform linux/amd64 -t $TAG .
+    set -x; docker build --load --platform linux/amd64 -t $TAG .; set +x
+
+    echo "Resulting CMD in image:"
+    docker image inspect debian-trixie-installer   | grep -A2 -i cmd
+
+    echo "Resulting files in image:"
+    #set -x; docker build --load --platform linux/amd64 -t $TAG --no-cache .; set +x
+    docker run --rm -it debian-trixie-installer find
 }
 
 case $(hostname) in
